34d0cb1c24331a86bdc599aa8d58777db9e24c2c
Akaam-Zain <akaamzain@hotmail.com>
2021-08-18 17:42:52 +0530

    Initial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SidebarOption from "./SidebarOption";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/** Twitter Icon */}
+      <TwitterIcon />
+      {/** SidebarOption */}
+      {/** SidebarOption */}
+      {/** SidebarOption */}
+      {/** SidebarOption */}
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      <SidebarOption />
+      {/** Twitter Icon */}
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SidebarOption.css";
+
+function SidebarOption(props) {
+  return (
+    <div className="sideBarOption">
+      <h2>This is an Option</h2>
+    </div>
+  );
+}
+
+export default SidebarOption;
